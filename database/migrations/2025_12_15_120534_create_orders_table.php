a8b28790965ee26bae9e8a6d902b99b4958b2f5d
Daniel Papenberg <daniel.papenberg@arvato.com>
2025-12-15 13:36:54 +0100

    Order Model & Migration

---
+++ database/migrations/2025_12_15_120534_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->enum('type', ['connector', 'vpn_connection']);
+            $table->enum('status', ['ordered', 'processing', 'completed'])->index();
+            $table->string('provider_order_id')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
